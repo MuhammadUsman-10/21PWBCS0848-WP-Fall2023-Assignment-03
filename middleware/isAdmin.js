686a0b2157922ec25ba0e9e1a47a7b3e934c91ce
Muhammad Usman <usman.aamir404@gmail.com>
2024-01-18 12:57:06 +0500

    message

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -5,19 +5,20 @@ const admin = require("../models/adminschema");
 const authAdmin = async (req, res, next) => {
   const token = req.header('Authorization');
 
-  if (!token) {
+  if (!token || !token.startsWith('Bearer')) {
     return res.status(401).json({ message: 'Access denied! Not an Admin' });
   }
 
   try {
     const secretkey= "#hhfu7%jhf!y90#gyo02%";
-    const decoded = jwt.verify(token, secretkey);
+    const tokenWithoutBearer = token.slice(7);
+    const decoded = jwt.verify(tokenWithoutBearer, secretkey);
     const admin = await admin.findById(decoded.admin._id);
     if (!admin){
       return res.status(500).json({message: "Admin not authenticated!"});
     }
     req.admin=admin;
-    if (req.admin.role !== 'admin') {
+    if (req.admin.role !== "admin") {
       return res.status(403).json({ message: 'Access denied. Not authorized as an admin.' });
     }
     next();
